ac57b2eda61efc23edf513292efaeff1ecb3f80e
whaleshade <whaleshade@gmail.com>
2023-01-31 21:32:40 +0900

    dblist.py

--- www/cgi-bin/dblist.py
+++ www/cgi-bin/dblist.py
@@ -3,24 +3,24 @@ from os import listdir
 from os.path import isfile, join
 from os import walk
 
-# upload_path = "/Users/jinhyeok/Desktop/42webserv/database/"
 upload_path = str(os.environ.get("UPLOAD_PATH"))
 
 files = [f for f in listdir(upload_path) if isfile(join(upload_path, f))]
 
-# print(onlyfiles)
-
 print ("<html>")
 print ("<body>")
-for item in files:
-	print(item)
-	item_path = "/database/" + item
-	print('''
-	<div>
-	<a href='%s' download>
- 	<button value="download" />
-	<img src='%s' alt='Download'>
-	</a></div>''' %(item_path, item_path))
+print("<div><a href='/home'>Go to home</a></div>")
+if files:
+	for item in files:
+		item_path = "/database/" + item
+		print('''
+		<li> %s
+		<a href='%s' download>
+		<button>DOWNLOAD</button>
+		</a></li>''' %(item, item_path))
+else:
+	print("<h2>NO FILES IN DB</h2>")
 
+print("<a href='upload'> upload to db</a>")
 print ("</body>")
 print ("</html>")

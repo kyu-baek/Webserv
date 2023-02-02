fb75fd28f250f256d0d8fdea1b425fdf2cf0a5c8
whaleshade <whaleshade@gmail.com>
2023-02-02 13:56:31 +0900

    login

---
+++ www/cgi-bin/join.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import cgi, os
+import cgitb
+import sqlite3
+
+upload_path = str(os.environ.get("UPLOAD_PATH"))
+
+def register_user(username, email, password):
+	conn = sqlite3.connect(upload_path + "/users.db")
+	c = conn.cursor()
+	c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, email TEXT, password TEXT)")
+	c.execute("SELECT * FROM users WHERE username=? OR email=?", (username, email))
+	if c.fetchone():
+		print("Error: The username or email already exists.")
+	else:
+		c.execute("INSERT INTO users (username, email, password) VALUES (?,?,?)", (username, email, password))
+		conn.commit()
+		print("Registration successful!")
+	conn.close()
+
+def show_user_list():
+	conn = sqlite3.connect(upload_path + "users.db")
+	c = conn.cursor()
+	c.execute("SELECT * FROM users")
+	print("<h2>Username / Email</h2>")
+	for row in c.fetchall():
+		print("<li>" + row[0] + " / " + row[1]+ "</li>")
+		print("<br>")
+	conn.close()
+
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+username = form.getvalue("username")
+email = form.getvalue("email")
+password = form.getvalue("password")
+confirm_password = form.getvalue("confirm_password")
+
+print("<html>")
+print("<head>")
+print("<title>Join Results</title>")
+print("</head>")
+print("<body>")
+print("<h1>Join Results</h1>")
+
+if username and email and password and confirm_password:
+	if password == confirm_password:
+		register_user(username, email, password)
+	else:
+		print("<p>Error: Passwords do not match.</p>")
+else:
+	print("<p>Error: All fields are required.</p>")
+
+print("<div>")
+show_user_list()
+print("</div>")
+
+print("</body>")
+print("</html>")
---
+++ www/cgi-bin/login.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import cgi, sqlite3,os
+import cgitb
+
+cgitb.enable()
+
+upload_path = str(os.environ.get("UPLOAD_PATH"))
+
+def validate(username, password):
+	conn = sqlite3.connect(upload_path +"users.db")
+	c = conn.cursor()
+	c.execute("SELECT * FROM users WHERE username=? AND password=?", (username, password))
+	user = c.fetchone()
+	if user:
+		return True
+	else:
+		return False
+
+form = cgi.FieldStorage()
+
+username = form.getvalue("username")
+password = form.getvalue("password")
+
+print("<html>")
+print("<body>")
+print("<h1>Login Results</h1>")
+
+if username and password:
+	# Validate the username and password here.
+	result = validate(username, password)
+	# Replace this with your own authentication logic.
+	if result == True:
+		print("<p>Login successful!</p>")
+	else:
+		print("<p>Login failed!</p>")
+else:
+	print("<p>Error: Both username and password are required.</p>")
+
+print("</body>")
+print("</html>")

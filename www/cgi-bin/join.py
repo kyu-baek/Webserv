35ac32a79584edf1695685fb67a527f32f96b1ad
whaleshade <whaleshade@gmail.com>
2023-02-02 17:29:27 +0900

    join.py session db

--- www/cgi-bin/join.py
+++ www/cgi-bin/join.py
@@ -6,15 +6,15 @@ import sqlite3
 
 upload_path = str(os.environ.get("UPLOAD_PATH"))
 
-def register_user(username, email, password):
+def register_user(username, email, password, id):
 	conn = sqlite3.connect(upload_path + "/users.db")
 	c = conn.cursor()
-	c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, email TEXT, password TEXT)")
+	c.execute("CREATE TABLE IF NOT EXISTS users (username TEXT, email TEXT, password TEXT, id TEXT)")
 	c.execute("SELECT * FROM users WHERE username=? OR email=?", (username, email))
 	if c.fetchone():
 		print("Error: The username or email already exists.")
 	else:
-		c.execute("INSERT INTO users (username, email, password) VALUES (?,?,?)", (username, email, password))
+		c.execute("INSERT INTO users (username, email, password, id) VALUES (?,?,?,?)", (username, email, password, id))
 		conn.commit()
 		print("Registration successful!")
 	conn.close()
@@ -25,7 +25,8 @@ def show_user_list():
 	c.execute("SELECT * FROM users")
 	print("<h2>Username / Email</h2>")
 	for row in c.fetchall():
-		print("<li>" + row[0] + " / " + row[1]+ "</li>")
+		# print("<li>" + row[0] + " / " + row[1]+ "</li>")
+		print(row)
 		print("<br>")
 	conn.close()
 
@@ -37,6 +38,7 @@ username = form.getvalue("username")
 email = form.getvalue("email")
 password = form.getvalue("password")
 confirm_password = form.getvalue("confirm_password")
+id = "default_id"
 
 print("<html>")
 print("<head>")
@@ -47,7 +49,7 @@ print("<h1>Join Results</h1>")
 
 if username and email and password and confirm_password:
 	if password == confirm_password:
-		register_user(username, email, password)
+		register_user(username, email, password, id)
 	else:
 		print("<p>Error: Passwords do not match.</p>")
 else:
